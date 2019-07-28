f9efaa6752b01a81bee35a94c87c2ba37ead43e5
null <null>
2019-07-28 10:23:08 +0300

    dev

---
+++ src/background.js
@@ -0,0 +1,18 @@
+'use strict';
+
+let timer;
+
+browser.browserAction.onClicked.addListener(async () => {
+    const downloads = await browser.downloads.search({limit: 1, orderBy: ['-startTime'], state: 'complete'});
+
+    if (downloads.length > 0) {
+        navigator.clipboard.writeText(downloads[0].filename);
+        browser.browserAction.setBadgeText({text: 'done'});
+    } else {
+        browser.browserAction.setBadgeText({text: 'fail'});
+    }
+    if (timer) {
+        clearTimeout(timer);
+    }
+    timer = setTimeout(() => browser.browserAction.setBadgeText({text: ''}), 1500);
+});
